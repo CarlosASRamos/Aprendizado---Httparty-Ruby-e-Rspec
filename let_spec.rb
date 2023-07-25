77f775b97285caa03667d7acf5a108e545606739
CarlosASRamos <140485214+CarlosASRamos@users.noreply.github.com>
2023-07-25 17:34:33 -0300

    Delete let_spec.rb

--- let_spec.rb
+++
@@ -1,9 +0,0 @@
-describe 'somar' do
-    context 'somar dois numeros' do
-        let(:resultado) {4 + 4}
-        let(:resultadoum) {resultado + 4}
-        it 'does something' do
-            puts resultadoum
-        end
-    end
-end
