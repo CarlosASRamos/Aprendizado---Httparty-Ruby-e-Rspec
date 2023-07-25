a4586e6fa7ddabd1a9906418a134368e8f23a091
CarlosASRamos <140485214+CarlosASRamos@users.noreply.github.com>
2023-07-25 17:34:15 -0300

    Delete get_spec.rb

--- get_spec.rb
+++
@@ -1,8 +0,0 @@
-describe 'fazer uma requisição' do
-    it 'get' do
-        @melhor_opcao Contato.get('/contacts')
-        puts @melhor_opcao
-    @primeira_opcao HTTParty.get('https://api-de-tarefas.herokuapp.com/contacts')
-    puts @primeira_opcao
-    end
-end
